9e70cc6b04c1fcbb9020b8ee4a89384fd3bcd464
Gabriel Saraiva <128630535+Gabriel-Saraiva-de-Gouveia@users.noreply.github.com>
2024-01-31 09:37:14 -0300

    Update First-Steps.py

--- Location/First-Steps.py
+++ Location/First-Steps.py
@@ -1,3 +1,3 @@
 from pygeocoder import Geocoder
-endereco = '552, Machado de Assis, Sarandi, PR'
+endereco = '20 W 34th St., New York, NY 10001, Estados Unidos'
 print(Geocoder('AIzaSyChn_ezkAEL7_AYyReDCP9cyCnAJsRbyN0').geocode(endereco).coordinates)
